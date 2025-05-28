49d4108211f4aed242eeca7202145ca944c8fae0
Gruszkajabluszka <Tomasz.misiewicz@outlook.com>
2025-05-28 10:20:19 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,4 +2,4 @@ class GameSettings:
     def __init__(self, rows=30, cols=30, speed=100):
         self.rows = rows
         self.cols = cols
-        self.speed = speed  # ms per frame
+        self.speed = speed 
